eb8b88d9c67095690d69b5acf42174e4ce8b6ac3
Maik Röder <maikroeder@gmail.com>
2012-12-21 21:29:28 +0100

    =extend tests and pep8

--- grape/recipe/pipeline/tests/test_prepare.py
+++ grape/recipe/pipeline/tests/test_prepare.py
@@ -17,53 +17,55 @@ from grape.recipe.pipeline.prepare import parse_flux_mem
 
 SANDBOX = tempfile.mkdtemp('buildoutSetUp')
 PATH = os.path.join(SANDBOX, 'buildout')
-OPTIONS = {
-    'accession': 'TestRun',
-    'location': os.path.join(PATH, 'parts/TestRun'),
-    }
-BUILDOUT = {
-    'TestRun': {
-        'paired':'1',
-        'file_location': '\n'.join([
-            os.path.join(PATH, "src/testdata/testA.r2.fastq.gz"),
-            os.path.join(PATH, "src/testdata/testA.r1.fastq.gz"),
-            os.path.join(PATH, "src/testdata/testB.r2.fastq.gz"),
-            os.path.join(PATH, "src/testdata/testB.r1.fastq.gz")]),
-        'species': 'Homo sapiens',
-        'readType': '2x76',
-        'cell': 'NHEK',
-        'rnaExtract': 'LONGPOLYA',
-        'localization': 'CELL',
-        'qualities': 'solexa',
-        'pair_id': 'testA\ntestA\ntestB\ntestB',
-        'mate_id': 'testA.2\ntestA.1\ntestB.2\ntestB.1',
-        'label': 'Test\nTest\nTest\nTest',
-       },
-    'pipeline': {
-         'TEMPLATE': os.path.join(
-             PATH,
-             "/src/pipeline/template3.0.txt"),
-         'PROJECTID': 'Test',
-         'THREADS': '2',
-         'DB': 'TestRNAseqPipeline',
-         'COMMONDB': 'TestRNAseqPipelineCommon',
-         'MAPPER': 'GEM',
-         'MISMATCHES': '2',
-         'GENOMESEQ': os.path.join(
-             PATH,
-             "/src/testdata/H.sapiens.genome.hg19.test.fa"),
-         'ANNOTATION': os.path.join(
-             PATH,
-             "/src/testdata/H.sapiens.EnsEMBL.55.test.gtf"),
-        },
-    'buildout': {'directory': PATH},
-    'settings': {
-         'java': '',
-         'flux_jar': '',
-         'overlap': os.path.join(PATH, 'src/overlap/overlap'),
-         'gem_folder': os.path.join(PATH, 'src/gem'),
-         }
-    }
+OPTIONS = {'accession': 'TestRun',
+           'location': os.path.join(PATH, 'parts/TestRun'),
+           }
+BUILDOUT = {}
+FILE_LOCATIONS = [os.path.join(PATH, "src/testdata/testA.r2.fastq.gz"),
+                  os.path.join(PATH, "src/testdata/testA.r1.fastq.gz"),
+                  os.path.join(PATH, "src/testdata/testB.r2.fastq.gz"),
+                  os.path.join(PATH, "src/testdata/testB.r1.fastq.gz")]
+BUILDOUT['TestRun'] = {'paired': '1',
+                       'file_location': '\n'.join(FILE_LOCATIONS),
+                       'species': 'Homo sapiens',
+                       'readType': '2x76',
+                       'cell': 'NHEK',
+                       'rnaExtract': 'LONGPOLYA',
+                       'localization': 'CELL',
+                       'qualities': 'solexa',
+                       'pair_id': 'testA\ntestA\ntestB\ntestB',
+                       'mate_id': 'testA.2\ntestA.1\ntestB.2\ntestB.1',
+                       'label': 'Test\nTest\nTest\nTest',
+                       'type': 'fastq',
+                       'replicate': '1'
+                       }
+TEMPLATE = os.path.join(PATH, "/src/pipeline/TEMPLATE3.0.txt")
+GENOME = os.path.join(PATH, "/src/testdata/H.sapiens.genome.hg19.test.fa")
+ANNOTATION = os.path.join(PATH, "/src/testdata/H.sapiens.EnsEMBL.55.test.gtf")
+BUILDOUT['pipeline'] = {'TEMPLATE': TEMPLATE,
+                        'PROJECTID': 'Test',
+                        'THREADS': '2',
+                        'DB': 'TestRNAseqPipeline',
+                        'COMMONDB': 'TestRNAseqPipelineCommon',
+                        'MAPPER': 'GEM',
+                        'MISMATCHES': '2',
+                        'GENOMESEQ': GENOME,
+                        'ANNOTATION': ANNOTATION,
+                        'MAXINTRONLENGTH': 50000,
+                        'CLUSTER': 'mem_6',
+                        'HOST': 'pou',
+                        'PREPROCESS': '${buildout:directory}/scripts/pre.pl',
+                        'PREPROCESS_TRIM_LENGTH': '24',
+                        'MIN_RECURSIVE_MAPPING_TRIM_LENGTH': '20',
+                        'FLUXMEM': '16G'
+                        }
+
+BUILDOUT['buildout'] = {'directory': PATH}
+BUILDOUT['settings'] = {'java': '',
+                        'flux_jar': '',
+                        'overlap': os.path.join(PATH, 'src/overlap/overlap'),
+                        'gem_folder': os.path.join(PATH, 'src/gem'),
+                        }
 SCRIPT = ["#!/bin/bash\nbin/start_RNAseq_pipeline.3.0.pl",
           "-species",
           "'Homo",
@@ -77,7 +79,7 @@ SCRIPT = ["#!/bin/bash\nbin/start_RNAseq_pipeline.3.0.pl",
           "-experiment",
           "TestRun",
           "-template",
-          "/src/pipeline/template3.0.txt",
+          "/src/pipeline/TEMPLATE3.0.txt",
           "-readlength",
           "76",
           "-cellline",
@@ -86,18 +88,34 @@ SCRIPT = ["#!/bin/bash\nbin/start_RNAseq_pipeline.3.0.pl",
           "LONGPOLYA",
           "-compartment",
           "CELL",
+          "-bioreplicate",
+          "1",
           "-threads",
           "2",
           "-qualities",
           "solexa",
+          "-cluster",
+          "mem_6",
           "-database",
           "TestRNAseqPipeline",
           "-commondb",
           "TestRNAseqPipelineCommon",
+          "-host",
+          "pou",
           "-mapper",
           "GEM",
           "-mismatches",
-          "2"]
+          "2",
+          "-maxintronlength",
+          "50000",
+          "-preprocess",
+          "'${buildout:directory}/scripts/pre.pl'",
+          "-preprocess_trim_length",
+          "24",
+          "-trimlength",
+          "20",
+          "-fluxmem",
+          "16G"]
 
 
 class MainTests(unittest.TestCase):
@@ -171,7 +189,7 @@ class MainTests(unittest.TestCase):
                                 'gem_folder': PATH,
                                 'nextgem_folder': PATH}
         result = main(OPTIONS.copy(), buildout)
-        self.failUnless(result == None)
+        self.failUnless(result is None)
 
 
 class PipelineScriptTests(unittest.TestCase):
@@ -256,86 +274,95 @@ class ReadLabelsTests(unittest.TestCase):
         """
         Test the check_read_labels method with a non existing paired attribute
         """
-        accession = {}
-        self.assertRaises(AttributeError, check_read_labels, accession, 'dummy')
+        acc = {}
+        self.assertRaises(AttributeError, check_read_labels, acc, 'dummy')
 
     def test_not_paired_dummy(self):
         """
         Test the check_read_labels method with non paired
         """
-        accession = {'paired':'0', 'pair_id':'', 'mate_id':'', 'label':''}
+        accession = {'paired': '0', 'pair_id': '', 'mate_id': '', 'label': ''}
         check_read_labels(accession, 'dummy')
 
     def test_paired_dummy(self):
         """
         Test the check_read_labels method with paired
         """
-        accession = {'paired':'1', 'pair_id':'1\n1', 'mate_id':'', 'label':''}
+        accession = {'paired': '1',
+                     'pair_id': '1\n1',
+                     'mate_id': '',
+                     'label': ''}
         check_read_labels(accession, 'dummy')
 
     def test_wrong_paired_dummy(self):
         """
         Test the check_read_labels method with paired
         """
-        accession = {'paired':'2', 'pair_id':'', 'mate_id':'', 'label':''}
-        self.assertRaises(AttributeError, check_read_labels, accession, 'dummy')
+        acc = {'paired': '2', 'pair_id': '', 'mate_id': '', 'label': ''}
+        self.assertRaises(AttributeError, check_read_labels, acc, 'dummy')
 
-    def test_not_paired_mate_and_pair_same(self):
+    def test_not_paired_mate_and_pair_same_1(self):
         """
         When not paired, mate_id and pair_id must be the same
         """
-        accession = {'paired':'0', 'pair_id':'1', 'mate_id':'2', 'label':''}
-        self.assertRaises(AttributeError, check_read_labels, accession, 'dummy')
+        acc = {'paired': '0', 'pair_id': '1', 'mate_id': '2', 'label': ''}
+        self.assertRaises(AttributeError, check_read_labels, acc, 'dummy')
 
-    def test_not_paired_mate_and_pair_same(self):
+    def test_not_paired_mate_and_pair_same_2(self):
         """
         All labels should be the same
         """
-        accession = {'paired':'0', 'pair_id':'1', 'mate_id':'1', 'label':'1\n2'}
-        self.assertRaises(AttributeError, check_read_labels, accession, 'dummy')
+        acc = {'paired': '0', 'pair_id': '1', 'mate_id': '1', 'label': '1\n2'}
+        self.assertRaises(AttributeError, check_read_labels, acc, 'dummy')
 
     def test_not_all_mate_ids_different(self):
         """
         All mate_ids must be different
         """
-        accession = {'paired':'0', 'pair_id':'1\n1', 'mate_id':'1\n1', 'label':'1\n1'}
-        self.assertRaises(AttributeError, check_read_labels, accession, 'dummy')
+        acc = {'paired': '0',
+               'pair_id': '1\n1',
+               'mate_id': '1\n1',
+               'label': '1\n1'}
+        self.assertRaises(AttributeError, check_read_labels, acc, 'dummy')
 
     def test_paired_where_pairs_not_paired_with_2(self):
         """
         Test the check_read_labels method where the pairs are not paired.
         Try two lines.
         """
-        accession = {'paired':'1', 'pair_id':'1\n2', 'mate_id':'', 'label':''}
-        self.assertRaises(AttributeError, check_read_labels, accession, 'dummy')
+        acc = {'paired': '1', 'pair_id': '1\n2', 'mate_id': '', 'label': ''}
+        self.assertRaises(AttributeError, check_read_labels, acc, 'dummy')
 
     def test_paired_where_pairs_not_paired_with_4(self):
         """
         Test the check_read_labels method where the pairs are not paired
         Try four lines.
         """
-        accession = {'paired':'1', 'pair_id':'1\n2\n3\n4', 'mate_id':'', 'label':''}
-        self.assertRaises(AttributeError, check_read_labels, accession, 'dummy')
+        acc = {'paired': '1',
+               'pair_id': '1\n2\n3\n4',
+               'mate_id': '',
+               'label': ''}
+        self.assertRaises(AttributeError, check_read_labels, acc, 'dummy')
 
     def test_parse_trim_length(self):
         """
         Test parsing of the trim length works for integers.
         """
-        pipeline = {'MIN_RECURSIVE_MAPPING_TRIM_LENGTH':'40'}
+        pipeline = {'MIN_RECURSIVE_MAPPING_TRIM_LENGTH': '40'}
         self.failUnless(parse_trim_length(pipeline) == '40')
-    
+
     def test_parse_flux_mem(self):
         """
         Test that an integer value for fluxmem works
         """
-        pipeline = {'FLUXMEM':'16'}
+        pipeline = {'FLUXMEM': '16'}
         self.failUnless(parse_flux_mem(pipeline) == '16')
 
     def test_parse_flux_mem_gigabyte(self):
         """
         Test that a number plus G works, like 16G returns 16.
         """
-        pipeline = {'FLUXMEM':'16'}
+        pipeline = {'FLUXMEM': '16'}
         self.failUnless(parse_flux_mem(pipeline) == '16')
 
 
